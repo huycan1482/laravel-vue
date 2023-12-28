a9ac41121e6e7f78b25ef3e392caad9c45fad74e
huydd <huydangduc@bizfly.vn>
2023-12-28 18:12:42 +0700

    f

---
+++ node_server/server.js
@@ -0,0 +1,48 @@
+// let io = require("socket.io")(6001);
+
+// console.log('Connecting to port ' + 6001);
+
+// io.on('error', function (socket) {
+//     console.log('error: ' + socket);
+// });
+
+// io.on('connection', function (socket) {
+//     console.log('connection: ' + socket.id);
+// });
+
+const express = require('express');
+const app = express();
+const httpServer = require("http").createServer(app);
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://laravel-vue.local.com",
+  },
+});
+
+io.on('connection', function (socket) {
+    console.log('connection: ' + socket.id);
+});
+
+httpServer.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// let Redis = require('ioredis');
+// let redis = new Redis(6379);
+ 
+// redis.psubscribe("*", function (error, count) {
+
+// });
+
+// redis.on('pmessage', function (partner, channel, message) {
+    
+//     message = JSON.parse(message);
+
+//     // console.log(partner);
+//     // console.log(channel);
+//     // console.log(message);
+
+//     io.emit(channel + ':' + message.event, message.data.message);
+//     console.log('DH send ', channel + ':' + message.event);
+// });
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -6,6 +6,7 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 import { useStore } from 'vuex'
+import { io } from "socket.io-client";
 
 export default {
     name: "App",
@@ -13,27 +14,43 @@ export default {
         
         const store = useStore()
         const accessToken = store.getters.authToken
-        window.Pusher = Pusher;
 
-        // socket.on('connect', () => {
-        //     console.log('socket connected: ' + socket.id);
-        //     socket.emit('join');
-        // });
-        window.Echo = new Echo({
-            broadcaster: 'socket.io',
-            // host: `${window.location.protocol}//${window.location.hostname}:6001`,
-            host: `${window.location.protocol}//${window.location.hostname}`,
 
-            // broadcaster: 'pusher',
-            // key: process.env.MIX_PUSHER_APP_KEY,
-            // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-            // encrypted: true,
-            auth: {
-                headers: {
-                    Authorization: 'Bearer ' + accessToken,
-                },
-            },
+        const URL = "http://laravel-vue.local.com:3000";
+        const socket = io(URL);
+
+        console.log("DH socket", socket);
+        socket.on('connect', () => {
+            console.log('socket connected: ' + socket.id);
+            // socket.emit('join', messageImportStatusRoom);
         });
+
+         socket.on("error", (error) => {
+            console.log('socket error', error);
+        })
+
+
+        // window.Pusher = Pusher;
+
+        // // socket.on('connect', () => {
+        // //     console.log('socket connected: ' + socket.id);
+        // //     socket.emit('join');
+        // // });
+        // window.Echo = new Echo({
+        //     broadcaster: 'socket.io',
+        //     // host: `${window.location.protocol}//${window.location.hostname}:6001`,
+        //     host: `${window.location.protocol}//${window.location.hostname}`,
+
+        //     // broadcaster: 'pusher',
+        //     // key: process.env.MIX_PUSHER_APP_KEY,
+        //     // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+        //     // encrypted: true,
+        //     auth: {
+        //         headers: {
+        //             Authorization: 'Bearer ' + accessToken,
+        //         },
+        //     },
+        // });
     }
 }
 </script>
